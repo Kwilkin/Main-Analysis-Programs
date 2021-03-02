8a2279321286c079d4fec73358b11c5b8044604f
Kwilkin <Kyle.j.wilkin@gmail.com>
2021-03-01 19:33:57 -0600

    Adding standard size
    Adding standard size to unpacking a quarter image. Will be used to combine images easier.

--- unpackQuarterImage.m
+++ unpackQuarterImage.m
@@ -1,4 +1,10 @@
-function [I] = unpackQuarterImage(QI);
+function [I] = unpackQuarterImage(QI,UseStandardSize);
+%% Rebuilds a quartered image.
+% Inputs:
+% QI: 2nd quadrant quarter of an image
+% UseStandardSize: 0 or 1. 
+% 0 means you use the whole image 
+% 1 means you use a standard size (useful for combining images)
 
 szY=size(QI,1);
 szX=size(QI,2);
@@ -7,7 +13,10 @@ I = zeros(2*szY-1,2*szX-1);
 
 % |1|2|
 % |3|4|
-I(1:szY,1:szX) = fliplr(QI); % q1
-I(1:szY,szX:end) = QI; % q2
-I(szY:end,1:szX) = rot90(QI,2); % q3
-I(szY:end,szX:end) = flipud(QI);
+if UseStandardSize
+else
+    I(1:szY,1:szX) = fliplr(QI); % q1
+    I(1:szY,szX:end) = QI; % q2
+    I(szY:end,1:szX) = rot90(QI,2); % q3
+    I(szY:end,szX:end) = flipud(QI);
+end
